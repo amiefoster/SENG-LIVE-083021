f03d7da480223ac9069e5e5396d62c69160f892d
Amie Foster <amie.n.foster@gmail.com>
2021-10-12 14:16:25 -0600

    done with lecture 1

--- phase-3/01-ruby-fundamentals/donation_tracker/lib/cli.rb
+++ phase-3/01-ruby-fundamentals/donation_tracker/lib/cli.rb
@@ -1,3 +1,38 @@
+
 def init
-  puts "init"
+  greeting
+  menu_list
 end 
+
+def greeting
+  puts "Welcome to the donation tracker app!"
+  puts "Please enter your name"
+  
+  name = gets 
+  puts "Awesome, #{name}, what would you like to do?"
+end
+
+def menu_list
+  puts "Please select from the following options"
+  puts "option 1"
+  puts "option 2"
+  puts "option 3"
+
+end
+
+def menu_selection
+  selection = gets.strip
+
+  while selection != "exit"
+    if selection == "1"
+      puts "create a donation"
+    elsif selection == ""
+      puts "show list of orgs"
+    elsif selection == "menu"
+      puts "menu again"
+    else
+      puts "Looks like something went wrong try again."
+    end
+    selection = gets.strip
+  end
+end
--- phase-3/01-ruby-fundamentals/exercise/lib/cli.rb
+++ phase-3/01-ruby-fundamentals/exercise/lib/cli.rb
@@ -1,9 +1,13 @@
+PIZZAS = []
+
 def init
   puts "Welcome to the lean, mean Pizza Machine App!"
   puts "Tell us your name: "
   user = gets.strip
   puts "Awesome, #{user.capitalize}, What would you like to do?"
   # need to invoke menu list methods
+  menu_options
+  menu_selection
   goodbye
 end 
 
@@ -12,10 +16,23 @@ def menu_options
   puts " 1. Create a new pizza!"
   puts " 2. View the list of created pizzas"
   puts " Change your mind? Type 'exit' to leave the app"
+  
 end 
 
 def menu_selection
   # Need to add some control flow based on users menu selection
+  selection = gets.strip
+  
+  until selection == "exit"
+    if selection == "1"
+      create_pizza
+    end
+      if delection == "2"
+        created_pizzas
+      end
+      menu_options
+      selection = gets.strip
+    end
 end 
 
 def create_pizza
@@ -27,11 +44,29 @@ def create_pizza
   desc = gets.strip 
 
   # need to return a pizza hash
+  pizza ={
+  name: name, 
+  topping: topping, 
+  desc: desc
+  }
+  PIZZAS << pizza
+
+  pizza
+
 end 
 
 # Currently there is not a method to print the list of created pizzas. In order to complete this:
 # 1. Think about a way to store and save the pizzas a user creates
 # 2. How can this collection be used to print a list of all of the stored pizzas
+
+def created_pizzas
+  PIZZAS.each do |pizza|
+    puts "Name: #{[:name]}"
+    puts "Toppings: #{[:toppings]}"
+    puts "Desc: #{[:desc]}"
+  end
+
+end
 
 def goodbye
   puts "Come back when you get hungry!!!"
